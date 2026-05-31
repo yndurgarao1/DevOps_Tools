5f42c40b0e69d51dd69a9925a75086acf86769d3
yndurgarao466 <yndurgarao@gmail.com>
2026-05-31 11:47:03 +0530

    with elastci ip security group

---
+++ AWS/Terraform/Terraform_Scripts/security_group_creation_eip.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "my-vpc" {
+    cidr_block = "25.30.0.0/16"
+    instance_tenancy = "default"
+    tags = {
+        Name = "my-vpc"
+    }
+}
+
+resource "aws_eip" "my_eip" {
+    tags = {
+        Name = "my-eip"
+    }
+}
+
+resource "aws_security_group" "sg_terraform" {
+    name        = "sg_terraform"
+    description = "Security group created by Terraform"
+    vpc_id      = aws_vpc.my-vpc.id
+    tags = {
+        Name = "SG-1-Terraform"
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["${aws_eip.my_eip.public_ip}/32"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks =  ["${aws_eip.my_eip.public_ip}/32"]
+    
+    }
+    
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks =  ["${aws_eip.my_eip.public_ip}/32"]
+ 
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
