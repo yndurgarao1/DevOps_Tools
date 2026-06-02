26826b4ddb3717ca1162b05176be223e05ad3a81
yndurgarao466 <yndurgarao@gmail.com>
2026-06-02 10:31:47 +0530

    random provider

--- AWS/Terraform/Terraform_Scripts/terraform_basics_scripts/local.tf
+++ AWS/Terraform/Terraform_Scripts/terraform_basics_scripts/local.tf
@@ -1,4 +1,4 @@
 resource "local_file" "my-file" {
-    filename = "/home/ubuntu/terraform_practise//file1.txt
-    content = "This is local resource practise
+    filename = "/home/ubuntu/terraform_practise//file1.txt"
+    content = "This is local resource practise"
 }
