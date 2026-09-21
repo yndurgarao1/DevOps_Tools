1908b8173df86faf1f9d5a58bb0baafc38244bc3
yndurgarao466 <yndurgarao@gmail.com>
2026-09-21 19:26:04 +0530

    terraform setup file

--- Kubernetes/Terraform_Setup/Terraform_Setup.sh
+++ Kubernetes/Terraform_Setup/Terraform_Setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 sudo su 
 yum update -y
-cd /usr/local/bin
+yum install -y git
 sudo wget https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_linux_386.zip
+mv terraform_1.15.8_linux_386.zip /usr/local/bin/
+cd /usr/local/bin
 unzip terraform_1.15.8_linux_386.zip
 cd /home/ec2-user
-cd DevOps_Tools/Terrform_EC2_Scripts
-cp Cluster_Setup.tf /home/ec2-user
+git clone https://github.com/yndurgarao1/DevOps_Tools.git
+sleep 60
+mkdir AWS_Terraform
+cp /home/ec2-user/DevOps_Tools/Kubernetes/Terraform_Setup/AWS_Provider.sh /home/ec2-user/AWS_Terraform
+cp /home/ec2-user/DevOps_Tools/Kubernetes/K8S_CLuster_Setup_Terraform_Code/K8S_Cluster_Setup.tf /home/ec2-user/AWS_Terraform
